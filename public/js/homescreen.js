9ae0eae201e568c87857df5b9cbfffa592785f1b
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-20 17:08:29 -0700

    Setup rendering of home page elements

---
+++ public/js/homescreen.js
@@ -0,0 +1,21 @@
+
+
+const rx = document.querySelector("#rxCabinet");
+const intro = document.querySelector("#intro");
+
+const renderIntro = () => {
+    intro.classList.add('active');
+}
+
+const renderRx = () => {
+    rx.classList.add('active');
+}
+
+
+const main = () => {
+    setTimeout(renderIntro, 10);
+    setTimeout(renderRx, 500)
+}
+
+
+main();
