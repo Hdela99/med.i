cde3deec2c597267b7c7878c41cd29cdbcff876e
Margot Cooper Gruen <margotgruen@gmail.com>
2022-09-21 01:52:33 -0700

    Added Javascript in login.js and changed ids to match login.handlebars. Also created blank signup.js file to fill in later.

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,22 @@
+const loginHandler = async (event) => {
+  event.preventDefault();
+  const email = document.querySelector("#floatingEmail").value.trim();
+  const password = document.querySelector("#floatingPassword").value.trim();
+  if (email && password) {
+    const loginResponse = await fetch("/api/user/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (loginResponse.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Login Failure!");
+    }
+  }
+};
+document.getElementById("loginForm").addEventListener("submit", loginHandler);
+
 //Ava - need to validate response object, user sends request, server says no
 //models is where we validate info from user.
 
