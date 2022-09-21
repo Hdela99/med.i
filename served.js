d13470c084baa910228293f9ec7181e017b256c1
hdela <hfdelacr@ucsd.edu>
2022-09-20 20:03:38 -0700

    finished the fetch calls for the two example fetches for data

--- served.js
+++ served.js
@@ -1,30 +1,30 @@
+require("dotenv").config();
 const fetch = require(`node-fetch`);
-var APIkey = "EEMqkuqHACLyxrKVimScECwbzR5PNHHab9HyTmZo";
-fetch(
-  `https://api.fda.gov/drug/event.json?api_key=EEMqkuqHACLyxrKVimScECwbzR5PNHHab9HyTmZo&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1`
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+var APIkey = process.env.API_KEY;
 
 async function getData() {
-  try {
-    const response = await fetch(
-      `https://api.fda.gov/drug/event.json?api_key=${APIkey}&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1`
-    );
-
-    if (!response.ok) {
-      throw new Error(`ERROR! you done messed up: ${response.status}`);
-    }
-
-    const result = await response.json();
-    return result;
-  } catch (err) {
-    console.log(err);
-  }
+  const response = await fetch(
+    `https://api.fda.gov/drug/event.json?api_key=${APIkey}&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
-
 getData();
+
+async function getInteraction(drug) {
+  const response = await fetch(
+    `https://api.fda.gov/drug/label.json?api_key=${APIkey}&search=drug_interactions:${drug}&count=openfda.substance_name.exact`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+getInteraction("hydrocodone");
+getInteraction("caffeine");
