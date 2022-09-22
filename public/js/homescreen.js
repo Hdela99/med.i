6bb418b1161e2848d819f8b62a3d0c14ff1abd2b
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-21 21:46:59 -0700

    Updated homepage to display carousel properly, started responsive design

--- public/js/homescreen.js
+++ public/js/homescreen.js
@@ -1,23 +1,54 @@
 
-
+// Instantiate all document items
 const rx = document.querySelector("#rxCabinet");
 const intro = document.querySelector("#intro");
+const carousel = document.querySelector("#rxCarousel")
+const carouselItems = document.querySelectorAll('.carousel .carousel-item')
+const smScreenItems = document.querySelector("#smallContent")
 
+
+console.log(smScreenItems)
 const renderIntro = () => {
     intro.classList.add('active');
 }
 
-const renderRx = () => {
-    rx.classList.add('active');
-}
-
-const underline = () => {
-    rx.classList.add('active');
+const removeChildren = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 const main = () => {
     setTimeout(renderIntro, 10);
-    setTimeout(renderRx, 500)
+    if (rx) {
+        setTimeout(() => rx.classList.add('active')
+            , 500)
+    }
+}
+
+// Configure carousel function to activate only if the screenwidth is >=1400px;
+if (screen.width >= 1440) {
+
+    removeChildren(smScreenItems);
+    carouselItems[0].classList.add('active');
+
+    carouselItems.forEach((el) => {
+
+        let minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = carouselItems[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+} else {
+    removeChildren(carousel);
+
 }
 
 
