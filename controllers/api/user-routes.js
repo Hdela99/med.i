5185f829a77b63ccedf6f1e16caf5b1971dd1f02
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-21 19:01:15 -0700

    Correcting merge error in login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,71 +4,57 @@ const { User } = require("../../models");
 // CREATE new user
 
 router.post('/', async (req, res) => {
-    try {
-        const newUserData = await User.create({
-            user_name: req.body.user_name,
-            email: req.body.email,
-            password: req.body.password
-        });
+  try {
+    const newUserData = await User.create({
+      user_name: req.body.user_name,
+      email: req.body.email,
+      password: req.body.password
+    });
 
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = newUserData.user_name;
-            req.session.email = newUserData.email;
-            req.session.uID = newUserData.id;
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userName = newUserData.user_name;
+      req.session.email = newUserData.email;
+      req.session.userID = newUserData.id;
 
-            res.status(200).json(newUserData);
-        })
+      res.status(200).json(newUserData);
+    })
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 
 })
 
 // LOGIN route
 router.post('/login', async (req, res) => {
-    try {
+  try {
 
-        const loginData = await User.findOne({
-            where: {
-                email: req.body.email,
-            },
-        })
+    const loginData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    })
 
-        if (!loginData) {
-            res.status(400).json({ message: 'Error, try again!' })
-            return
-        }
-
-        const pwCheck = await loginData.pwVerification(req.body.password);
-
-        if (!pwCheck) {
-            res.status(400).json({ message: 'Error, incorrect password!' });
-            return;
-        }
-
-        req.session.save(() => {
-            req.session.loggedIn = true;
-            req.session.userId = loginData.user_name;
-            req.session.email = loginData.email;
-            req.session.uID = loginData.id;
-
-
+    if (!loginData) {
+      res.status(400).json({ message: 'Error, try again!' })
+      return
+    }
 
     const pwCheck = await loginData.pwVerification(req.body.password);
 
     if (!pwCheck) {
-      res.status(400).json({ message: "Error, incorrect password!" });
+      res.status(400).json({ message: 'Error, incorrect password!' });
       return;
     }
 
+
     req.session.save(() => {
       req.session.loggedIn = true;
-      req.session.userId = loginData.user_name;
+      req.session.userName = loginData.user_name;
       req.session.email = loginData.email;
-      req.session.uID = loginData.id;
+      req.session.userID = loginData.id;
 
       res.status(200).json({ username: loginData, message: "Login Success!" });
     });
