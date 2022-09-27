6319892093478754cc4e68bd31538a6681304214
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-26 20:21:30 -0700

    Implemented initial modal formatting

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,9 +1,14 @@
 
-
+$("#modal").iziModal();
 
 $(document).on('click', '.trigger', function (event) {
     event.preventDefault();
     // $('#modal').iziModal('setZindex', 99999);
+    // $('#modal').iziModal('iframeHeight', 250);
+
     // $('#modal').iziModal('open', { zindex: 99999 });
-    $('#modal').iziModal('open');
+    $('#modal').iziModal('open', {
+        autoOpen: true,
+        title:'Alert!!!'
+    });
 });
