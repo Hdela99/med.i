2588a5e609d378dfc28ac6a3d9be61b4693cbfbf
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-26 21:21:08 -0700

    Temporary example of data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const withAuth = require("../utils/auth");
-
+const apiKey = process.env.API_KEY;
+require("dotenv").config();
+const fetch = require("node-fetch");
 // Renders the main page
 //NEEDS WITHAUTH
 
@@ -18,8 +20,9 @@ router.get("/", async (req, res) => {
     //   }
     //   ]
     // });
-    // const posts = postData.map(post => post.get({ plain: true }));
+    // const medicine = medicineData.map(post => post.get({ plain: true }));
     res.render("home", {
+      // medicine,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
@@ -44,9 +47,36 @@ router.get("/signup", async (req, res) => {
 });
 //NEEDS WITHAUTH
 
+// router.get("/search", async (req, res) => {
+//   try {
+//     res.render("search", {
+//       loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.get("/search", async (req, res) => {
   try {
+    // FE JS takes the input queries, processes using the search-routes we have established
+    let drug = req.body.drug;
+    let url = `https://api.fda.gov/drug/event.json?api_key=${apiKey}&search=ibuprofen&count=patient.reaction.reactionmeddrapt.exact&limit=10`;
+
+    // Formatting data
+    const drugFx = await fetch(url).then((res) => {
+      return res.json();
+    });
+    // let drugFxArr = drugFx.results.map((element) => {
+    //   return element.term;
+    // });
+
+
+    let drugFxArr = drugFx.results
+    console.log(drugFxArr)
+    // Passes it to the res.render for whatever our results should look like
     res.render("search", {
+      drugFxArr,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
