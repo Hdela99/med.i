63e625b09eb7cfaf63af65bd08c7a325b4ab7621
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-26 17:51:23 -0700

    Saving modal progression

---
+++ public/js/modal.js
@@ -0,0 +1,9 @@
+
+
+
+$(document).on('click', '.trigger', function (event) {
+    event.preventDefault();
+    // $('#modal').iziModal('setZindex', 99999);
+    // $('#modal').iziModal('open', { zindex: 99999 });
+    $('#modal').iziModal('open');
+});
