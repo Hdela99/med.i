e6540af040e6c2f563e22a86768cd86b7dea84d1
MarioArmstrong <arms.mario@icloud.com>
2022-09-26 19:39:02 -0800

    re-configured the seeds files because they were not entirely working correctly

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,31 +1,19 @@
 const sequelize = require("../config/connection");
 const { User, Comment, Medication } = require("../models");
+const seedMedication = require("./medicationData");
+const seedUser = require("./userData");
+const seedComment = require("./commentData");
 
-const userData = require("./user.json");
-const commentData = require("./comment.json");
-const medicationData = require("./medication.json");
-
-const seedDatabase = async () => {
+const seedAll = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  await seedUser();
 
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
+  await seedMedication();
 
-    // for (const medication of medicationData) {
-    //   await Medication.create({
-    //     ...medication,
-    //     user_id: users[Math.floor(Math.random() * users.length)].id,
-    //   });
-    // }
-  }
+  await seedComment();
+
+  process.exit(0);
 };
 
-seedDatabase();
+seedAll();
