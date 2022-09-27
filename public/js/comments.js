f46261224d4e4608cfbb8c3fa30a895415758d61
Margot Cooper Gruen <margotgruen@gmail.com>
2022-09-26 20:18:57 -0700

    work for monday.

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,4 +1,10 @@
-const postComment = async (event) => {
+const { post } = require("../../controllers/home-routes");
+
+const commentHandler = async (event) => {
   event.preventDefault();
-  const comment = document.querySelector().value.trim();
+  const title = document.getElementsByClassName(commentTitle).value.trim();
+  const body = document.getElementsByClassName(commentContents).value.trim();
+  if (title && body) {
+    const commentInfo = post("")
 };
+
