34915f3972c0983d54c96b1b8685d74ab664ed0a
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-26 19:08:59 -0700

    Removed excess comments

--- controllers/api/search-routes.js
+++ controllers/api/search-routes.js
@@ -49,22 +49,6 @@ router.post("/", async (req, res) => {
   }
 
 
-
-
-  // console.log("----------------------");
-  // console.log(req.body);
-  // Medication.create({
-  //   medication_name: req.body.medication_name,
-  //   adverse_effects: req.body.adverse_effects,
-  //   route_of_medication: req.body.route_of_medication,
-  // }).then((medData) => {
-  //   req.session.save(() => {
-  //     req.session.medication_name = medData.medication_name;
-  //     req.session.adverse_effects = medData.adverse_effects;
-  //     req.session.route_of_medication = medData.route_of_medication;
-  //     res.status(200).json(medData);
-  //   });
-  // });
 });
 
 // TEST ROUTE THIS SHOULD BE DELETED LATER
