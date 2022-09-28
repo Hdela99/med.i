642498099191a3a15f140d2d43e274912ba5acd6
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-27 19:19:26 -0700

    Fixed comments functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -29,8 +29,9 @@ router.get("/:id", async (req, res) => {
 //POST route Comment
 router.post("/", async (req, res) => {
   try {
-    console.log(req.body)
 
+    req.body.user_id = req.session.userID;
+    console.log(req.body);
     const commentData = await Comment.create(req.body);
     res.status(200).json(commentData);
   } catch (err) {
--- public/js/comments.js
+++ public/js/comments.js
@@ -1,36 +1,23 @@
-<<<<<<< HEAD
+
+
+
 const commentHandler = async (event) => {
-  console.log("clicked");
-  // event.preventDefault();
-  const title = event.target[0].value.trim();
-  const body = event.target[1].value.trim();
-  if (title && body) {
-    const commentInfo = await fetch("/api/comment-routes/", {
+  const comment = event.target[0].value;
+  const medication_id = document.getElementById("rx-name").dataset.value;
+
+  if (comment) {
+    const commentInfo = await fetch("/api/comment/", {
       method: "POST",
-      body: JSON.stringify({ title, body }),
+      body: JSON.stringify({ comment, medication_id }),
       headers: { "Content-Type": "application/json" },
     });
 
-    console.log(commentInfo);
   }
 };
 
 document.querySelector("#tacocat").addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(event);
+
   commentHandler(event);
 });
 //send to backend after capture as object
-=======
-
-
-
-const commentHandler = async (event) => {
-  event.preventDefault();
-  const title = document.getElementsByClassName(commentTitle).value;
-  const body = document.getElementsByClassName(commentContents).value;
-  if (title && body) {
-    const commentInfo = post("")
-  }
-};
->>>>>>> e62ac52fdd47b6dd9d10cab5f20a7193616b0d15
